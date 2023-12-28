8de5d7f32743f7a60fc7de6d1be0c4f47534600a
ojcarlos <carlos.ojunior00@gmail.com>
2023-12-28 17:13:42 -0400

    project and mainpackages have been created

---
+++ src/main/java/oj/carlos/sneakercounter/SneakerCounterApplication.java
@@ -0,0 +1,13 @@
+package oj.carlos.sneakercounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SneakerCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SneakerCounterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oj/carlos/sneakercounter/SneakerCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package oj.carlos.sneakercounter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SneakerCounterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
